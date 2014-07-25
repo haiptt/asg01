a2a3090d5204a5cff5f2a9a160d9418860dfc817
haiptt <haiphamtuan010192@gmail.com>
2014-07-25 23:41:50 +0700

    no message

---
+++ SinhVien.java
@@ -0,0 +1,11 @@
+public class SinhVien{
+    public static void main(String[] args){
+System.out.println("Pham Tuan Hai 11020110 K55CD haiphamtuan010192@gmail.com");
+//int i;
+           for(int i=9;i>1;i--){
+               System.out.println("bottles of beer on the wall,"+i);
+System.out.println("bottles of beer. Take one down, pass it around, ");
+           }
+
+}
+} 
